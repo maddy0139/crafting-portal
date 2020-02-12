3af81a5178f7136cc1321b960b59b1cab6aad90a
unknown <mahendra0139@gmail.com>
2020-02-13 00:45:36 +0530

    Updates

---
+++ precache-manifest.5e217283df2aa561f96a5e987f4c78de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5279265e2a3bed9d8902a1ad2e00990",
+    "url": "/crafting-portal/index.html"
+  },
+  {
+    "revision": "441233668fd9bb8ea125",
+    "url": "/crafting-portal/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f266e23740e404b8a5b9",
+    "url": "/crafting-portal/static/css/main.fd81e1b9.chunk.css"
+  },
+  {
+    "revision": "441233668fd9bb8ea125",
+    "url": "/crafting-portal/static/js/2.2707927d.chunk.js"
+  },
+  {
+    "revision": "3cf623170d2e6ffcb643963f13e5959b",
+    "url": "/crafting-portal/static/js/2.2707927d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f266e23740e404b8a5b9",
+    "url": "/crafting-portal/static/js/main.ae9bf8af.chunk.js"
+  },
+  {
+    "revision": "69b0baf082a9fb873f8e",
+    "url": "/crafting-portal/static/js/runtime-main.eca684d1.js"
+  }
+]);
