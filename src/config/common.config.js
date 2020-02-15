9c0df4c1a420fcf5277c9d9e6684852fce4e6269
maddy0139 <mahendra0139@gmail.com>
2020-02-16 00:26:49 +0530

    added hash router and dependency

--- src/config/common.config.js
+++ src/config/common.config.js
@@ -1,3 +1,3 @@
 export default {
-    BASE_URL: 'http://localhost:8080/api'
+    BASE_URL: 'http://crafting-portal-node-api-crafting-portal.apps.ca-central-1.starter.openshift-online.com/api'
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Main/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Store,{persistor} from './store/configureStore';
 import { PersistGate } from 'redux-persist/integration/react'
