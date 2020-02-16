06935f5ddf30b8a6c498ccef31f6f7c3c9c7be56
maddy0139 <mahendra0139@gmail.com>
2020-02-16 11:49:33 +0530

    change navbar

--- src/components/common/Header/Header.component.tsx
+++ src/components/common/Header/Header.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import store from '../../../store/configureStore';
-import UserApi from "../../../apis/user-api/UserApi";
+import UserApi from '../../../apis/user-api/UserApi';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, NavItem } from 'react-bootstrap';
 import logo from '../../../assets/images/icons8-gift-100.png';
@@ -9,77 +9,103 @@ import logo from '../../../assets/images/icons8-gift-100.png';
 const userApi = new UserApi(store);
 
 interface IHeaderProps {
-    userInformation: any
-    logout: any
+  userInformation: any;
+  logout: any;
 }
 
 class HeaderComponent extends React.Component<IHeaderProps, {}> {
-
-    constructor(props: IHeaderProps) {
-        super(props);
-        this.logoutClickHandler = this.logoutClickHandler.bind(this);
-    }
-    logoutClickHandler(e: React.MouseEvent) {
-        this.props.logout();
-
-    }
-    onNavDropDownSelect() {
-        
-    }
-    render() {
-        const {userInformation} = this.props;
-        return (
-
-            <>
-                <Navbar bg="primary" variant="dark" fixed="top" >
-                    <Navbar.Brand as={Link} to="/home">
-                        <img
-                            alt=""
-                            src={logo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        Feelings Throught Crafting
-                    </Navbar.Brand>
-                    <Navbar.Collapse className="justify-content-end">
-                        {userInformation &&
-                            <Nav className="navbar-right">
-                                <NavDropdown title={`${userInformation.firstName} ${userInformation.lastName}`} id="basic-nav-dropdown">
-                                    <NavDropdown.Item as={Link} to="/home">Home</NavDropdown.Item>
-                                    <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
-                                    <NavDropdown.Item as={Link} to="/settings">Settings</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#" onClick={(e: any) => this.logoutClickHandler(e)}>Logout</NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link></Nav.Link>
-                            </Nav>
-
-                        }
-                        {!userInformation &&
-                            <Nav>
-                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                                <Nav.Link as={Link} eventKey={2} to="/signup">Signup</Nav.Link>
-                            </Nav>
-                        }
-                    </Navbar.Collapse>
-                </Navbar>
-            </>
-
-        );
-    }
+  constructor(props: IHeaderProps) {
+    super(props);
+    this.logoutClickHandler = this.logoutClickHandler.bind(this);
+  }
+  logoutClickHandler(e: React.MouseEvent) {
+    this.props.logout();
+  }
+  onNavDropDownSelect() {}
+  render() {
+    const { userInformation } = this.props;
+    return (
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="primary"
+          variant="dark"
+          fixed="top"
+        >
+          <Navbar.Brand as={Link} to="/home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Feelings Throught Crafting
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            {userInformation && (
+              <Nav className="mr-auto">
+                <NavDropdown
+                  title={`${userInformation.firstName} ${
+                    userInformation.lastName
+                  }`}
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item as={Link} to="/home">
+                    Home
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/profile">
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/settings">
+                    Settings
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    href="#"
+                    onClick={(e: any) => this.logoutClickHandler(e)}
+                  >
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link />
+              </Nav>
+            )}
+            {!userInformation && (
+              <Nav>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} eventKey={2} to="/signup">
+                  Signup
+                </Nav.Link>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }
 }
 
 function mapStateToProps(state: any, ownProps: any) {
-    return {
-        userInformation: userApi.getUserDetails()
-    };
+  return {
+    userInformation: userApi.getUserDetails()
+  };
 }
 function mapDispatchToProps(dispatch: any) {
-    return {
-        logout: () => {
-            return userApi.logout()
-        }
-    };
+  return {
+    logout: () => {
+      return userApi.logout();
+    }
+  };
 }
-export default connect(mapStateToProps, mapDispatchToProps)(HeaderComponent);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HeaderComponent);
--- src/components/common/Menu/Menu.component.tsx
+++ src/components/common/Menu/Menu.component.tsx
@@ -1,56 +1,62 @@
 import React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import store from '../../../store/configureStore';
-import UserApi from "../../../apis/user-api/UserApi";
+import UserApi from '../../../apis/user-api/UserApi';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, NavItem } from 'react-bootstrap';
-import logo from '../../../assets/images/icons8-gift-100.png';
 
 const userApi = new UserApi(store);
 
-
 class MenuComponent extends React.Component {
-    render() {
-        return (
-            <>
-                <Navbar expand="lg" bg="dark" variant="dark">
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </>
-
-        );
-    }
+  render() {
+    return (
+      <>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }
 }
 
 function mapStateToProps(state: any, ownProps: any) {
-    return {
-        userInformation: userApi.getUserDetails()
-    };
+  return {
+    userInformation: userApi.getUserDetails()
+  };
 }
 function mapDispatchToProps(dispatch: any) {
-    return {
-        logout: () => {
-            return userApi.logout()
-        }
-    };
+  return {
+    logout: () => {
+      return userApi.logout();
+    }
+  };
 }
-export default connect(mapStateToProps, mapDispatchToProps)(MenuComponent);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MenuComponent);
